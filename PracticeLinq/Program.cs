0aefc3b2f39309cd3c206e2b69de28a69b998a79
Katherine Winters <goble.katherine@gmail.com>
2021-12-22 12:31:52 -0600

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGames = new List<string>
+            {
+                "Valhiem", "New World", "Outward", "Divinity", "Balders Gate"
+            };
+
+            IEnumerable<string> result = videoGames.OrderByDescending(game => game.Length);
+
+            foreach (string game in result)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
